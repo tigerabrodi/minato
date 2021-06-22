dc2f58877d372434c58f62ed66bbbbaa95fd0e65
tigerabrodi <tigerabrodi@gmail.com>
2021-06-22 17:56:26 +0200

    chore: add theme and setup client

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+import { createClient, Provider } from 'urql'
+
+const client = createClient({
+  url: 'https://api.yelp.com/v3/graphql',
+  fetchOptions: {
+    headers: {
+      Authorization: `Bearer ${process.env.REACT_APP_KEY}`,
+    },
+  },
+})
+
 export const App = () => {
-  return <div>Hello World</div>
+  return (
+    <Provider value={client}>
+      <div>Hello World</div>
+    </Provider>
+  )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { setup, theme } from 'twind'
+import { css } from 'twind/css'
 import { App } from './App'
+
+setup({
+  preflight: (preflight) => css`
+    ${preflight}
+    body {
+      background-color: ${theme('colors.white')};
+    }
+    div#root {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      font-family: ${theme('fontFamily.roboto')};
+    }
+  `,
+  theme: {
+    extend: {
+      colors: {
+        red: 'D32323',
+        black: '000000',
+        white: 'FFFFFF',
+      },
+      fontFamily: {
+        roboto: 'Roboto',
+      },
+    },
+    screens: {
+      phone: '425px',
+      tablet: '768px',
+      desktop: '1024px',
+      laptop: '1440px',
+    },
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
