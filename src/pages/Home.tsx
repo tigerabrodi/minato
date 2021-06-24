37a468a6dcaa6313edfe33c2c269e5ff4dbb7573
tigerabrodi <tigerabrodi@gmail.com>
2021-06-24 18:55:16 +0200

    feat(Home): handle submit when search button is clicked

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
+import * as React from 'react'
 import { tw } from 'twind'
 import { css } from 'twind/css'
 import { ReactComponent as SearchSVG } from 'assets/search.svg'
+import { useHistory } from 'react-router-dom'
 
 const containerStyles = css`
   height: calc(100vh - 8rem);
@@ -12,11 +14,22 @@ const containerStyles = css`
 `
 
 export const Home = () => {
+  const history = useHistory()
+  const [food, setFood] = React.useState('')
+  const [city, setCity] = React.useState('')
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    history.push(`/burgers?food=${food}&city=${city}`)
+  }
+
   return (
     <main className={tw(containerStyles)}>
       <h1 className={tw`text-7xl font-bold tablet:(text-9xl) pt-4`}>Minato</h1>
       <form
         className={tw`h-2/3 w-full flex flex-col items-center justify-evenly tablet:(h-1/2)`}
+        onSubmit={handleSubmit}
       >
         <div
           className={tw`h-8 w-72 bg-current rounded-sm flex flex-row items-center justify-evenly tablet:(h-10 w-96)`}
@@ -30,6 +43,8 @@ export const Home = () => {
           <input
             type="text"
             id="food"
+            value={food}
+            onChange={(event) => setFood(event.target.value)}
             placeholder="Burger, Cheesecake, Pizza..."
             className={tw`text(sm gray-100) tablet:(text-base) focus:outline-none bg-transparent placeholder-opacity-50 w-10/12 h-full pl-5`}
           />
@@ -47,6 +62,8 @@ export const Home = () => {
             type="text"
             id="city"
             placeholder="Berlin, New York, Tokyo..."
+            value={city}
+            onChange={(event) => setCity(event.target.value)}
             className={tw`text(sm gray-100) tablet:(text-base) focus:outline-none bg-transparent placeholder-opacity-50 w-10/12 h-full pl-5`}
           />
         </div>
